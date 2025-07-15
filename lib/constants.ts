f5f7d936eff5f50c038b4aca9e78cd4053ed764a
Chibuike <amadichibuike72@gmail.com>
2025-07-15 13:59:29 +0100

    updated the constant for deployed url

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,8 @@
 export const STUDENT_API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://student-information-management-system-sigma.vercel.app/api/students"
+    ? "https://student-management-system-phi-one.vercel.app/api/students"
     : "http://localhost:3000/api/students";
 export const AUTH_API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://student-information-management-system-sigma.vercel.app/api/auth"
+    ? "https://student-management-system-phi-one.vercel.app/api/auth"
     : "http://localhost:3000/api/auth";
